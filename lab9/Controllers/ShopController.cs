18a477bc02d0124f071281abb48190b9bd6410ba
annakepowicz <ankakepowicz123@gmail.com>
2026-01-10 18:32:14 +0100

    Add Cart logic and views

--- lab9/Controllers/ShopController.cs
+++ lab9/Controllers/ShopController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
 using lab9.Models;
 
 namespace lab9.Controllers
@@ -30,6 +31,90 @@ namespace lab9.Controllers
             var articles = await articlesQuery.ToListAsync();
 
             return View(articles);
+        }
+
+        [HttpPost]
+        public IActionResult AddToCart(int id)
+        {
+            string cookieKey = "basket_" + id;
+            int count = 1;
+
+            if (Request.Cookies.ContainsKey(cookieKey))
+            {
+                if (int.TryParse(Request.Cookies[cookieKey], out int existingCount))
+                {
+                    count = existingCount + 1; 
+                }
+            }
+
+            CookieOptions options = new CookieOptions
+            {
+                Expires = DateTime.Now.AddDays(7),
+                HttpOnly = true, 
+                IsEssential = true
+            };
+
+            Response.Cookies.Append(cookieKey, count.ToString(), options);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Cart()
+        {
+            var cartItems = new List<CartItem>();
+            
+            foreach (var cookie in Request.Cookies)
+            {
+                if (cookie.Key.StartsWith("basket_"))
+                {
+                    if (int.TryParse(cookie.Key.Replace("basket_", ""), out int articleId))
+                    {
+                        var article = await _context.Articles
+                            .Include(a => a.Category)
+                            .FirstOrDefaultAsync(a => a.Id == articleId);
+
+                        if (article != null)
+                        {
+                            int quantity = int.Parse(cookie.Value);
+                            cartItems.Add(new CartItem { Article = article, Quantity = quantity });
+                        }
+                    }
+                }
+            }
+
+            return View(cartItems);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateQuantity(int id, int delta)
+        {
+            string cookieKey = "basket_" + id;
+
+            if (Request.Cookies.ContainsKey(cookieKey))
+            {
+                if (int.TryParse(Request.Cookies[cookieKey], out int currentCount))
+                {
+                    int newCount = currentCount + delta;
+
+                    if (newCount <= 0)
+                    {
+                        // Usuwamy ciasteczko, jeśli ilość to 0 lub mniej
+                        Response.Cookies.Delete(cookieKey);
+                    }
+                    else
+                    {
+                        // Aktualizujemy ciasteczko na kolejne 7 dni
+                        Response.Cookies.Append(cookieKey, newCount.ToString(), new CookieOptions
+                        {
+                            Expires = DateTime.Now.AddDays(7),
+                            HttpOnly = true,
+                            IsEssential = true
+                        });
+                    }
+                }
+            }
+
+            return RedirectToAction(nameof(Cart));
         }
     }
 }
---
+++ lab9/Models/CartItem.cs
@@ -0,0 +1,8 @@
+namespace lab9.Models
+{
+    public class CartItem
+    {
+        public Article Article { get; set; }
+        public int Quantity { get; set; }
+    }
+}
